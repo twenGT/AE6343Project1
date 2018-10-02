f5a9d30de20fe35e2a60906c273143d0f10ad817
Terry Wen <TerryWen@Xiaomings-MacBook-Pro.local>
2018-10-02 01:39:07 -0400

    Template
    Template from the TAs

---
+++ Report/LaTeX_Run_Commands.bat
@@ -0,0 +1,4 @@
+pdflatex -synctex=1 -interaction=nonstopmode Report.tex
+bibtex Report
+pdflatex -synctex=1 -interaction=nonstopmode Report.tex
+pdflatex -synctex=1 -interaction=nonstopmode Report.tex
