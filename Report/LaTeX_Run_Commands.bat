b6ff759bfb2b96f546ed0e6b0148621cc6c45d78
Terry Wen <TerryWen@Xiaomings-MacBook-Pro.local>
2018-10-03 00:48:49 -0400

    Delete LaTex Template
    Switching to Overleaf

--- Report/LaTeX_Run_Commands.bat
+++
@@ -1,4 +0,0 @@
-pdflatex -synctex=1 -interaction=nonstopmode Report.tex
-bibtex Report
-pdflatex -synctex=1 -interaction=nonstopmode Report.tex
-pdflatex -synctex=1 -interaction=nonstopmode Report.tex
